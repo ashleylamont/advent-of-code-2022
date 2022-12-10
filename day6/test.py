c4a577846e27cbdf757f89252546b7fd0b22f631
Ashley Lamont <ashley@ashl.dev>
2022-12-10 18:24:23 +1100

    Removed inputs

--- day6/test.py
+++ day6/test.py
@@ -5,42 +5,42 @@ from day6.signal_stream import SignalStream
 
 class SignalStreamPacketStartPositionTest(unittest.TestCase):
     def test_sample_case_1(self):
-        signal_stream = SignalStream("bvwbjplbgvbhsrlpgdmjqwftvncz")
-        self.assertEqual(5, signal_stream.get_start_packet_position())
+        signal_stream = SignalStream("INPUT REMOVED AS PER AOC RULES")
+        self.assertEqual(0, signal_stream.get_start_packet_position())
 
     def test_sample_case_2(self):
-        signal_stream = SignalStream("nppdvjthqldpwncqszvftbrmjlhg")
-        self.assertEqual(6, signal_stream.get_start_packet_position())
+        signal_stream = SignalStream("INPUT REMOVED AS PER AOC RULES")
+        self.assertEqual(0, signal_stream.get_start_packet_position())
 
     def test_sample_case_3(self):
-        signal_stream = SignalStream("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
-        self.assertEqual(10, signal_stream.get_start_packet_position())
+        signal_stream = SignalStream("INPUT REMOVED AS PER AOC RULES")
+        self.assertEqual(0, signal_stream.get_start_packet_position())
 
     def test_sample_case_4(self):
-        signal_stream = SignalStream("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
-        self.assertEqual(11, signal_stream.get_start_packet_position())
+        signal_stream = SignalStream("INPUT REMOVED AS PER AOC RULES")
+        self.assertEqual(0, signal_stream.get_start_packet_position())
 
 
 class SignalStreamPacketMessagePositionTest(unittest.TestCase):
     def test_sample_case_1(self):
-        signal_stream = SignalStream("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
-        self.assertEqual(19, signal_stream.get_start_message_position())
+        signal_stream = SignalStream("INPUT REMOVED AS PER AOC RULES")
+        self.assertEqual(0, signal_stream.get_start_message_position())
 
     def test_sample_case_2(self):
-        signal_stream = SignalStream("bvwbjplbgvbhsrlpgdmjqwftvncz")
-        self.assertEqual(23, signal_stream.get_start_message_position())
+        signal_stream = SignalStream("INPUT REMOVED AS PER AOC RULES")
+        self.assertEqual(0, signal_stream.get_start_message_position())
 
     def test_sample_case_3(self):
-        signal_stream = SignalStream("nppdvjthqldpwncqszvftbrmjlhg")
-        self.assertEqual(23, signal_stream.get_start_message_position())
+        signal_stream = SignalStream("INPUT REMOVED AS PER AOC RULES")
+        self.assertEqual(0, signal_stream.get_start_message_position())
 
     def test_sample_case_4(self):
-        signal_stream = SignalStream("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
-        self.assertEqual(29, signal_stream.get_start_message_position())
+        signal_stream = SignalStream("INPUT REMOVED AS PER AOC RULES")
+        self.assertEqual(0, signal_stream.get_start_message_position())
 
     def test_sample_case_5(self):
-        signal_stream = SignalStream("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
-        self.assertEqual(26, signal_stream.get_start_message_position())
+        signal_stream = SignalStream("INPUT REMOVED AS PER AOC RULES")
+        self.assertEqual(0, signal_stream.get_start_message_position())
 
 
 if __name__ == '__main__':
--- day7/scratch.py
+++
@@ -1,6 +0,0 @@
-from day7.file_system import FileSystem
-
-with open('input', 'r') as file:
-    lines = file.readlines()
-    fs = FileSystem(lines)
-    print(fs)
